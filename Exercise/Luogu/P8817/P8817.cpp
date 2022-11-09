bded8ff7c05d0fc4b8ea13d2eb9f80e7ca16c83f
Lotuses-robot <robot7833@vip.qq.com>
2022-11-09 19:05:38 +0800

    Remove execute files

--- Exercise/Luogu/P8817/P8817.cpp
+++ Exercise/Luogu/P8817/P8817.cpp
@@ -31,9 +31,11 @@ struct Node {
 std::queue<Node> q;
 int dis[3001][3001];
 bool vis[3001];
+std::pair<int, int> ans[3001][2];
 
-void bfs(int st) {
+void bfs1(int st) {
     memset(vis, 0, sizeof(vis));
+    memset(dis[st], 63, sizeof(dis[st]));
     q.push({st, 0});
     dis[st][st] = 0;
     while (!q.empty()) {
@@ -42,7 +44,28 @@ void bfs(int st) {
             if (!vis[e.to]) {
                 vis[e.to] = true;
                 dis[st][e.to] = p.dis + 1;
+                // printf("%d %d %d\n", st, e.to, dis[st][e.to]);
                 q.push({e.to, p.dis + 1});
+            }
+        }
+    }
+}
+
+int sc[3030];
+
+void bfs2() {
+    for (int i = 2; i <= n; i++) {
+        for (int j = 2; j <= n; j++) {
+            if (i != j && dis[1][i] <= k && dis[i][j] <= k) {
+                // puts("fk");
+                if (ans[j][0].first < sc[i] + sc[j]) {
+                    ans[j][1] = ans[j][0];
+                    ans[j][0].first = sc[i] + sc[j];
+                    ans[j][0].second = i;
+                } else if (ans[j][1].first < sc[i] + sc[j]) {
+                    ans[j][1].first = sc[i] + sc[j];
+                    ans[j][1].second = i;
+                }
             }
         }
     }
@@ -50,6 +73,9 @@ void bfs(int st) {
 
 int main() {
     read(n, m, k);
+    for (int i = 2; i <= n; i++) {
+        read(sc[i]);
+    }
     for (int i = 0; i < m; i++) {
         int u, v;
         read(u, v);
@@ -57,11 +83,31 @@ int main() {
         G[v].push_back({v, u});
     }
 
+    std::pair<int, int> init; init.first = init.second = -1;
     for (int i = 1; i <= n; i++) {
-        bfs(i);
+        bfs1(i);
+        ans[i][0] = ans[i][1] = init;
     }
+    // printf("%d %d\n", dis[1][2], dis[2][3]);
+    bfs2();
 
-    for (int i = 1; i <= n; i++) {
-        bfs(i);
+    int anss = -1;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 2; j <= n; j++) {
+            if (i != j && dis[i][j] <= k) {
+                for (int a = 0; a < 2; a++) {
+                    for (int b = 0; b < 2; b++) {
+                        if (ans[i][a].second == j
+                            || ans[i][a].second == ans[j][b].second
+                            || ans[j][b].second == i) continue;
+                        anss = std::max(anss, ans[i][a].first
+                                            + ans[j][b].first);
+                        printf("%d %d %d %d %d\n", i, a, j, b, anss);
+                    }
+                }
+            }
+        }
     }
+    printf("%d\n", anss);
+    return 0;    
 }
