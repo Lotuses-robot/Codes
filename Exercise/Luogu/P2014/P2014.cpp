23cc6806e4adf98d4c62dd2a486992345b275ed5
Lotuses-robot <robot7833@vip.qq.com>
2023-07-10 14:40:03 +0800

    upd

--- Exercise/Luogu/P2014/P2014.cpp
+++ Exercise/Luogu/P2014/P2014.cpp
@@ -24,41 +24,32 @@ void writeln(T arg, Ts...arg_left) { write(arg); putchar(' '); write(arg_left...
 #define debug(arg, args...) {}
 #endif
 
-const int maxn = 105;
+const int maxn = 305;
 struct Edge {
-    int u, v, w;
+    int u, v;
 };
 std::vector<Edge> G[maxn];
+int w[maxn];
+int f[maxn][maxn];
+int n, m;
 
-void insert(int u, int v, int w) {
-    G[u].push_back({u, v, w});
-    G[v].push_back({v, u, w});
+void insert(int u, int v) {
+    G[u].push_back({u, v});
+    G[v].push_back({v, u});
 }
 
-int f[maxn][maxn];
-int fas[maxn];
-
-int dfs(int cur, int q, int fa) {
-    fas[cur] = fa;
-    if (f[cur][q]) return f[cur][q];
-    if (G[cur].size() == 1) {
-        if (q >= 1) return -998244353;
-        else return 0;
-    }
-    int ans = -998244353;
-    int ss[2], ww[2], cnt = 0;
-    for (auto e : G[cur]) {
+void dfs(int u, int fa) {
+    f[u][1] = w[u];
+    for (auto e : G[u]) {
         if (e.v != fa) {
-            ss[cnt] = e.v;
-            ww[cnt] = e.w;
-            cnt++;
+            dfs(e.v, u);
+            for (int k = m; k >= 1; k--) {
+                for (int j = 0; j < k; j++) {
+                    f[u][k] = std::max(f[u][k], f[u][k - j] + f[e.v][j]);
+                }
+            }
         }
     }
-    ans = std::max(dfs(ss[0], q - 1, cur) + ww[0], dfs(ss[1], q - 1, cur) + ww[1]);
-    for (int j = 0; q - j - 2 >= 0; j++) {
-        ans = std::max(ans, dfs(ss[0], j, cur) + dfs(ss[1], q - j - 2, cur) + ww[0] + ww[1]);
-    }
-    return f[cur][q] = ans;
 }
 
 int main() {
@@ -66,17 +57,16 @@ int main() {
         freopen(".in", "r", stdin);
         freopen(".out", "w", stdout);
     #endif
-    
-    int n, q;
-    read(n, q);
-    for (int i = 1; i < n; i++) {
-        int u, v, w;
-        read(u, v, w);
-        insert(u, v, w);
-    }
 
-    fas[1] = -1;
-    int ans = dfs(1, q, fas[1]);
-    writeln(ans);
+    read(n, m); m++;
+    
+    for (int i = 1; i <= n; i++) {
+        static int x;
+        read(x, w[i]);
+        insert(i, x);
+    }
+    
+    dfs(0, -1);
+    writeln(f[0][m]);
     return 0;
 }
